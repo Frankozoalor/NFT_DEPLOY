8e4f3099bf572138f5a3014a2159d32d736c95a8
EkeneOzoalor <88558745+EkeneOzoalor@users.noreply.github.com>
2022-03-13 00:07:44 -0800

    nft deploy

---
+++ contracts/NFT.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTee is ERC721 {
+
+    constructor() ERC721("ZALOR-NFT", "ZALOR"){
+
+        _mint(msg.sender, 1);
+    }
+
+}
